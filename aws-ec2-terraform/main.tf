71a791ab54e9ef233d55e3abe4a46adae9d6a87e
Davi Oliveira <novaes.davi1@gmail.com>
2024-07-09 14:25:07 -0300

    removing profile

--- aws-ec2-terraform/main.tf
+++ aws-ec2-terraform/main.tf
@@ -1,55 +1,54 @@
 
-  provider "aws" {
-    profile    = "default"
-    region     = "us-east-1"
-    access_key = var.AWS_ACCESS_KEY_ID
-    secret_key = var.AWS_SECRET_ACCESS_KEY
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+}
+
+resource "aws_instance" "app_server" {
+  count                  = 2
+  ami                    = "ami-06c68f701d8090592"
+  instance_type          = "t2.micro"
+  key_name               = "terraform-key-pair"
+  vpc_security_group_ids = [aws_security_group.main.id]
+
+  tags = {
+    Name = "First-Ec2-With-Terraform"
   }
+  connection {
+    type    = "ssh"
+    host    = self.public_ip
+    user    = "ec2-user"
+    timeout = "4m"
+  }
+}
 
-  resource "aws_instance" "app_server" {
-    count                  = 2
-    ami                    = "ami-06c68f701d8090592"
-    instance_type          = "t2.micro"
-    key_name               = "terraform-key-pair"
-    vpc_security_group_ids = [aws_security_group.main.id]
-
-    tags = {
-      Name = "First-Ec2-With-Terraform"
+resource "aws_security_group" "main" {
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
     }
-    connection {
-      type    = "ssh"
-      host    = self.public_ip
-      user    = "ec2-user"
-      timeout = "4m"
+  ]
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
     }
-  }
-
-  resource "aws_security_group" "main" {
-    egress = [
-      {
-        cidr_blocks      = ["0.0.0.0/0", ]
-        description      = ""
-        from_port        = 0
-        ipv6_cidr_blocks = []
-        prefix_list_ids  = []
-        protocol         = "-1"
-        security_groups  = []
-        self             = false
-        to_port          = 0
-      }
-    ]
-    ingress = [
-      {
-        cidr_blocks      = ["0.0.0.0/0", ]
-        description      = ""
-        from_port        = 22
-        ipv6_cidr_blocks = []
-        prefix_list_ids  = []
-        protocol         = "tcp"
-        security_groups  = []
-        self             = false
-        to_port          = 22
-      }
-    ]
-  }
+  ]
+}
 
