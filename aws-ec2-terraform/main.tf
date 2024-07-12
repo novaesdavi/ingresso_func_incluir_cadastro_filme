ad294965c7679980d1804a94b7ea20561fe73b4f
Davi Oliveira <novaes.davi1@gmail.com>
2024-07-12 10:55:44 -0300

    voltando pro que funciona

--- aws-ec2-terraform/main.tf
+++ aws-ec2-terraform/main.tf
@@ -1,4 +1,14 @@
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+  required_version = ">= 1.0.4"
+}
+
 provider "aws" {
   region     = "us-east-1"
   access_key = var.AWS_ACCESS_KEY_ID
