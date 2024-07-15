14c60e8d5544e2559a416deddebfa8dcd883e2af
Davi Oliveira <novaes.davi1@gmail.com>
2024-07-15 20:40:29 -0300

    ajustando terraform incluindo lambda

--- aws-ec2-terraform/main.tf
+++ aws-ec2-terraform/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.27"
+      version = "~> 5.0"
     }
   }
   required_version = ">= 1.0.4"
@@ -15,21 +15,42 @@ provider "aws" {
   secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
-resource "aws_instance" "app_server" {
-  count                  = 0
-  ami                    = "ami-06c68f701d8090592"
-  instance_type          = "t2.micro"
-  key_name               = "terraform-key-pair"
-  vpc_security_group_ids = [aws_security_group.main.id]
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_ingresso_incrluir_cadastro_role"
 
-  tags = {
-    Name = "First-Ec2-With-Terraform"
-  }
-  connection {
-    type    = "ssh"
-    host    = self.public_ip
-    user    = "ec2-user"
-    timeout = "4m"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "my_lambda" {
+  function_name = "IncluirCadastroFilmeFunction"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "IncluirCadastroFilmeFunction::IncluirCadastroFilmeFunction.Function::FunctionHandler"
+  runtime       = "dotnet8"
+
+  # Assuming your Lambda code is in a file called "lambda_function.zip"
+  filename         = "../src/IncluirCadastroFilmeFunction/bin/Release/net8.0/IncluirCadastroFilmeFunction.zip"
+  source_code_hash = filebase64sha256("../src/IncluirCadastroFilmeFunction/bin/Release/net8.0/IncluirCadastroFilmeFunction.zip")
+
+  environment {
+    variables = {
+      # Add any environment variables your Lambda function needs here
+    }
   }
 }
 
@@ -47,18 +68,4 @@ resource "aws_security_group" "main" {
       to_port          = 0
     }
   ]
-  ingress = [
-    {
-      cidr_blocks      = ["0.0.0.0/0", ]
-      description      = ""
-      from_port        = 22
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "tcp"
-      security_groups  = []
-      self             = false
-      to_port          = 22
-    }
-  ]
 }
-
--- src/IncluirCadastroFilmeFunction/Function.cs
+++ src/IncluirCadastroFilmeFunction/Function.cs
@@ -1,21 +1,27 @@
+using System.Text.Json.Serialization;
 using Amazon.Lambda.Core;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
-[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+[assembly : LambdaSerializer (typeof (Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
 namespace IncluirCadastroFilmeFunction;
 
-public class Function
-{
-    
+public class Function {
+
     /// <summary>
     /// A simple function that takes a string and does a ToUpper
     /// </summary>
     /// <param name="input">The event for the Lambda function handler to process.</param>
     /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
     /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
-    {
-        return input.ToUpper();
+    public string FunctionHandler (Filme input, ILambdaContext context) {
+        return input.Titulo;
     }
+}
+
+[JsonSerializable (typeof (Filme))]
+public class Filme {
+    public string Titulo { get; set; }
+    public int Ano { get; set; }
+    public string Diretor { get; set; }
 }
