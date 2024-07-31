c6fe804a0a4ce16d36cdf0f7b001fccbcdb913f3
Davi Oliveira <novaes.davi1@gmail.com>
2024-07-31 00:26:31 -0300

    masi testes

--- aws-ec2-terraform/main.tf
+++ aws-ec2-terraform/main.tf
@@ -50,7 +50,7 @@ resource "aws_iam_role_policy_attachment" "lambda_policy" {
 
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_dir  = "./publish"
+  source_dir  = "./ingresso_func_incluir_cadastro_filme/publish"
   output_path = "./IncluirCadastroFilmeFunction.zip"
 }
 
