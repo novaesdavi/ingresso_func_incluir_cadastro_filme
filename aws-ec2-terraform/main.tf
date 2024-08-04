dac28f367d739707a15a84083a6ff4dbb1204514
Davi Oliveira <novaes.davi1@gmail.com>
2024-08-04 18:34:55 -0300

    adicionando setup

--- aws-ec2-terraform/main.tf
+++ aws-ec2-terraform/main.tf
@@ -51,7 +51,7 @@ resource "aws_iam_role_policy_attachment" "lambda_policy" {
 data "archive_file" "lambda_zip" {
   type        = "zip"
   source_dir  = path.module
-  output_path = ${path.module}/${local.zip_file}"
+  output_path = "${path.module}/IncluirCadastroFilmeFunction.zip"
 }
 
 
