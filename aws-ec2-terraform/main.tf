059f5c94b3196b53eb7335cfe0f4822fe06ae917
Davi Oliveira <novaes.davi1@gmail.com>
2024-08-05 23:35:25 -0300

    teste

--- aws-ec2-terraform/main.tf
+++ aws-ec2-terraform/main.tf
@@ -20,8 +20,9 @@ terraform {
 }
 
 provider "aws" {
-  access_key = ""
-  secret_key = ""
+  region     = "us-east-1"
+  access_key = env.AWS_ACCESS_KEY_ID
+  secret_key = env.AWS_SECRET_ACCESS_KEY
 }
 
 
