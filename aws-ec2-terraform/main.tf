efaf215caa80c22c7fcdeae39b99b0eb588d004b
Davi Oliveira <novaes.davi1@gmail.com>
2024-08-07 00:38:11 -0300

    teste

--- aws-ec2-terraform/main.tf
+++ aws-ec2-terraform/main.tf
@@ -47,6 +47,30 @@ resource "null_resource" "force_deploy" {
   }
 }
 
+resource "random_pet" "lambda_bucket_name" {
+  prefix = "ingresso-lambda-bucket"
+  length = 4
+}
+
+
+resource "aws_s3_bucket" "lambda_bucket" {
+  bucket = random_pet.lambda_bucket_name.id
+}
+
+resource "aws_s3_bucket_ownership_controls" "lambda_bucket" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "lambda_bucket" {
+  depends_on = [aws_s3_bucket_ownership_controls.lambda_bucket]
+
+  bucket = aws_s3_bucket.lambda_bucket.id
+  acl    = "private"
+}
+
 
 resource "aws_s3_object" "lambda_s3_ingresso" {
   bucket = aws_s3_bucket.lambda_bucket.id
