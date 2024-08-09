232ea0b3ed31dc4b59db2014ff910d979c202711
Davi Oliveira <novaes.davi1@gmail.com>
2024-08-08 21:55:30 -0300

    incluindo bloco variable

--- aws-ec2-terraform/main.tf
+++ aws-ec2-terraform/main.tf
@@ -62,13 +62,6 @@ resource "aws_lambda_function" "my_lambda" {
   filename      = data.archive_file.lambda_zip.output_path
   depends_on    = [null_resource.force_deploy]
 
-  environment {
-    variables = {
-      AWS_REGION            = "us-east-1",
-      AWS_ACCESS_KEY_ID     = env.AWS_ACCESS_KEY_ID,
-      AWS_SECRET_ACCESS_KEY = env.AWS_SECRET_ACCESS_KEY
-    }
-  }
 }
 
 
