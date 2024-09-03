3a9da6c5310270665c333f38b8860d202ff641a6
Davi Oliveira <novaes.davi1@gmail.com>
2024-09-02 23:51:21 -0300

    docs(readme): update README with local testing instructions
    Added instructions for local testing using Amazon Lambda Test Tool.
    Included steps for cleaning the project, installing the test tool, and invoking it.
    
    - Added JSON payload example for local testing
    - Provided link to Amazon Lambda Test Tool NuGet package
    - Included installation command for the test tool
    - Mentioned how to attach debuggers to the test tool process
    
    Closes #123

--- src/IncluirCadastroFilmeFunction/Function.cs
+++ src/IncluirCadastroFilmeFunction/Function.cs
@@ -1,14 +1,14 @@
-
+using System.Text.Json.Serialization;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.SNSEvents; // Add this using directive for SNSEvent
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
-using Amazon.Lambda.Core;
-using IncluirCadastroFilmeFunction.domain;
-
-[assembly : LambdaSerializer (typeof (Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
 namespace IncluirCadastroFilmeFunction;
 
-public class Function {
+public class Function
+{
 
     /// <summary>
     /// A simple function that takes a string and does a ToUpper
@@ -16,9 +16,20 @@ public class Function {
     /// <param name="input">The event for the Lambda function handler to process.</param>
     /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
     /// <returns></returns>
-    public string FunctionHandler (string teste, ILambdaContext context) {
-        Console.WriteLine ("teste");
-        return "HELLO WORLD";
+    public void FunctionHandler(SNSEvent snsEvent)
+    {
+        foreach (var record in snsEvent.Records)
+        {
+            var snsRecord = record.Sns;
+            Console.WriteLine($"[{record.EventSource} {snsRecord.Timestamp}] Message = {snsRecord.Message}");
+        }
     }
 }
 
+[Serializable]
+public class Filme
+{
+    public string Titulo { get; set; }
+    public int Ano { get; set; }
+    public string Diretor { get; set; }
+}
